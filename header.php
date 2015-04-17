e37c516fea7f327275230b379319faddd829f19c
NrdyN8 <nathan@haletek.com>
2015-04-16 23:49:25 -0600

    Started CSS for entire app

--- header.php
+++ header.php
@@ -10,8 +10,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
+        <link rel="stylesheet" type="text/css" href="./CSS/main.css"/>
 	</head>
 	<body>
-        <button value="Log Out">Log Out</button>
-        <button value="Back to Search">Back to Search</button>
-        <?php $dbconnector->getUser(); ?>
+        <div id="header">
+            <button value="Log Out">Log Out</button>
+            <button value="Back to Search">Back to Search</button>
+            <?php $dbconnector->getUser(); ?>
+        </div>
