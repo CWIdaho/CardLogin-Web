2ff0970741f19f5992e2d56c19852927a7b6dce4
NrdyN8 <nathan@haletek.com>
2015-04-22 11:45:25 -0600

    Upload of DB credentials

--- DB/dbcon.php
+++ DB/dbcon.php
@@ -1,8 +1,8 @@
 <?php
 	//Do not fill these in then push to repo!
 	//Make sure these are empty when you do a push!
-	define("_DBNAME", "");
-	define("_IP", "");
-	define("_HOST", "");
-	define("_USER", "");
-	define("_PASSWORD", "");
+	define("_DBNAME", "MATSDB");
+	define("_IP", "172.30.2.2");
+	define("_HOST", "TESTSERVER");
+	define("_USER", "sa");
+	define("_PASSWORD", "password");
